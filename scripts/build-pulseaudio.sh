55a8a911ed8f83a2150a7ee7ad56d06cb118e3b4
Michal Vittek <35069784+mivittek@users.noreply.github.com>
2024-10-20 11:12:56 +0200

    Update build-pulseaudio.sh
    edited namespace to mvittek and librespot version to 0.5.0

--- scripts/build-pulseaudio.sh
+++ scripts/build-pulseaudio.sh
@@ -38,8 +38,8 @@ function create_and_push_manifest() {
   docker manifest push $NAME:$TAG
 }
 
-LIBRESPOT_VERSION="0.4.2"
-DOCKER_NAMESPACE="tmigone"
+LIBRESPOT_VERSION="0.5.0"
+DOCKER_NAMESPACE="mvittek"
 
 build_and_push_image "Dockerfile.pulseaudio.template" "${DOCKER_NAMESPACE}/librespot:$LIBRESPOT_VERSION-pulseaudio-rpi" "rpi" "linux/arm/v6" "$LIBRESPOT_VERSION"
 build_and_push_image "Dockerfile.pulseaudio.template" "${DOCKER_NAMESPACE}/librespot:$LIBRESPOT_VERSION-pulseaudio-armv7hf" "armv7hf" "linux/arm/v7" "$LIBRESPOT_VERSION"
